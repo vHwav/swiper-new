5d08a49ea0b7cbc9308890fac7a46c305c365335
vHwav <dandivan0708@gmail.com>
2023-12-26 04:23:08 +0900

    폴더 위치 변경

---
+++ docs/js/main.js
@@ -0,0 +1,31 @@
+const swiperBox = Array.from(document.querySelectorAll(".ad-imgs-box"));
+
+const swiperImage = document.querySelector(".ad-img");
+const closeButton = Array.from(document.querySelectorAll(".close"));
+
+// console.log(imgBoxStyles);
+console.log(swiperBox[0].classList);
+
+function handleClick(e) {
+  let targetImg = e.target.closest(".swiper-slide");
+  let targetIndex = targetImg.dataset.swiperSlideIndex;
+  console.log(targetIndex);
+  if (!targetImg) return;
+
+  for (let i = 0; i < swiperBox.length; i++) {
+    if (targetIndex == i) {
+      swiperBox[i].classList.add("is-active");
+    }
+  }
+}
+function handleClose() {
+  swiperBox.forEach((item) => {
+    return item.classList.remove("is-active");
+  });
+}
+
+swiperImage.addEventListener("click", handleClick);
+
+closeButton.forEach((item) => {
+  item.addEventListener("click", handleClose);
+});
---
+++ docs/js/swiper.js
@@ -0,0 +1,88 @@
+// const rootStyles = window.getComputedStyle(document.documentElement);
+// const adFigure = document.querySelector(".ad-figure");
+
+var swiper = new Swiper(".ad-swiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".custom-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".custom-button-next",
+    prevEl: ".custom-button-prev",
+  },
+  on: {
+    // activeIndexChange: function () {
+    //   if (this.activeIndex === 1) {
+    //     adFigure.style.setProperty("--theme-color", "#5BC0F8");
+    //   }
+    // },
+    // activeIndexChange: function () {
+    //   if (this.activeIndex === 2) {
+    //     adFigure.style.setProperty("--theme-color", "#2FA007");
+    //   }
+    // },
+    // activeIndexChange: function () {
+    //   if (this.activeIndex === 2) {
+    //     adFigure.style.setProperty("--theme-color", "#FFC368");
+    //   }
+    // },
+  },
+});
+
+var swiper4 = new Swiper(".ad-imgs-sea", {
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -800],
+      rotate: [180, 0, 0],
+    },
+    next: {
+      shadow: true,
+      translate: [0, 0, -800],
+      rotate: [-180, 0, 0],
+    },
+  },
+});
+
+var swiper4 = new Swiper(".ad-imgs-mountain", {
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -800],
+      rotate: [180, 0, 0],
+    },
+    next: {
+      shadow: true,
+      translate: [0, 0, -800],
+      rotate: [-180, 0, 0],
+    },
+  },
+});
+
+var swiper4 = new Swiper(".ad-imgs-desert", {
+  grabCursor: true,
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -800],
+      rotate: [180, 0, 0],
+    },
+    next: {
+      shadow: true,
+      translate: [0, 0, -800],
+      rotate: [-180, 0, 0],
+    },
+  },
+});
